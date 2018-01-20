5e7216a1611df47d42ea517cff183985b5ec3899
KingOfDemon <peteothai@gmail.com>
2018-01-19 18:33:47 -0600

    Add custom msg

--- gorgon_node/scripts/gorgon.py
+++ gorgon_node/scripts/gorgon.py
@@ -1,5 +1,24 @@
-#!/usr/env/python
+#!/usr/bin/env python
+import rospy
+from gorgon_node.msg import custom
+
+def sendToGorgon(string):
+	rospy.loginfo("Tx: " + str(string))
+
+def setSpeed(message):
+	value = int(((message.speed+1)/2.0)*255)
+	sendToGorgon("M " + str(message.MotorID) + " " + str(value))
+
 
 if __name__ == "__main__":
     # Entry point
+    rospy.init_node('gorgon_node')
+
+    rate = rospy.Rate(10) # 10hz
+    rospy.Subscriber("Motor_speed",custom, setSpeed)
+    while not rospy.is_shutdown():
+	rate.sleep()
+rospy.spin()
+
+#Create message to controll individual motor
 
