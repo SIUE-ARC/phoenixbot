a23a8c6e07b5032ddf97c23c59197652db8189b7
Kade Walter <kadewalter1@gmail.com>
2018-01-19 18:09:54 -0600

    encoder now takes the value and divides the int by the encoder resolution

--- gorgon_node/scripts/gorgon.py
+++ gorgon_node/scripts/gorgon.py
@@ -7,9 +7,13 @@ def sendToGorgon(string):
 	rospy.loginfo("Sending " + str(string) + " to gorgon")
 
 def readFromGorgon():
-	rospy.loginfo("Reading from gorgon")
+#	rospy.loginfo("Reading from gorgon")
 	return "1024" 
-
+	
+def getResolution(i):
+	resolution = rospy.get_param("enc" + str(i) + "/resolution")
+	return resolution
+	
 def readAnalog():
 	values = range(0,5)
 	# Ask the gorgon what the analog values are
@@ -24,17 +28,26 @@ def readAnalog():
 	analog_input_pub.publish(message)
 
 def readEncoder():
-	values = range(0,8)
+	encoder = range(0,8)
 	# Ask the gorgon what the analog values are
 	for i in range(0,8):
-		sendToGorgon("E " + str(i))
-		values[i] = readFromGorgon()
-	values = map(int, values)
+		#sendToGorgon("E " + str(i))
+		encoder[i] = readFromGorgon()
+		encoder = map(int, encoder)
+		rospy.loginfo(encoder[i])
+	#convert to radians (float)
+	for i in range(0,8):
+		res = getResolution(i)
+		if(res == 0):
+			res = 1
+			encoder[i] = (encoder[i] * 0)
+		encoder[i] = encoder[i]/res
+		rospy.loginfo(encoder[i])
 
 	# Publish those values to nodes that are listening
-	message = Int32MultiArray()
-	message.data = values
-	encoder_input_pub.publish(message)
+#	message = Int32MultiArray()
+#	message.data = values
+#	encoder_input_pub.publish(message)
 
 if __name__ == "__main__":
     # Entry point
