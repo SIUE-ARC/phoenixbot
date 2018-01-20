f081d0c357bd1b7aa2b2179591cca7fb29bbd1af
bwither@siue.edu <bwither@siue.edu>
2018-01-19 18:35:05 -0600

    Changed the way we send the message

--- robot_node/scripts/robot_node.py
+++ robot_node/scripts/robot_node.py
@@ -20,13 +20,14 @@ def callback(data):
 	right_motor_speed = turnspeed - drivespeed
 	print("Left motor speed: {}".format(left_motor_speed))
 	print("Right motor speed: {}".format(right_motor_speed))
-	pub = rospy.Publisher("motor_speed", String, queue_size=1)
-	message = "{},{}".format(left_motor_speed,right_motor_speed)
-	pub.publish(message)
+	left_motor_message = "{},{}".format(get_left_motor_param,left_motor_speed)
+	right_motor_message = "{},{}".format(get_right_motor_param,right_motor_speed)
+	pub.publish(left_motor_message)
+	pub.publish(right_motor_message)
 
 
 rospy.Subscriber("cmd_vel", Twist, callback)
-
+pub = rospy.Publisher("motor_speed", String, queue_size=2)
 
 
 rospy.spin()
