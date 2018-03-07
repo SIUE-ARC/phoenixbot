260f5104bb25caf468a7470f20a0c0cece01d2bd
Zach Anderson <zach.inbox@gmail.com>
2018-03-07 15:43:24 -0600

    Using waypoints file

--- phoenixbot_behavior_coordination/scripts/phoenixbot_behavior_coordinator.py
+++ phoenixbot_behavior_coordination/scripts/phoenixbot_behavior_coordinator.py
@@ -5,7 +5,9 @@ import numpy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseActionResult
 from sensor_msgs.msg import Imu
+from std_srvs.srv import Empty
 import tf.transformations
 
 z_hat = numpy.array([0,0,1]);
@@ -30,10 +32,46 @@ def imu_callback(data):
     else:
         ramp = False;
 
-    print(deviation, ramp)
+active_goal_status = 0
+def nav_callback(msg):
+    global active_goal_status
+    active_goal_status = msg.status
 
 def wait_for_start():
     pass
+
+def get_waypoint(waypoint_name):
+    pose = PoseStamped()
+    pose.header.frame_id = "map"
+    pose.pose.position.x = rospy.get_param('~{}/position/x'.format(waypoint_name))
+    pose.pose.position.y = rospy.get_param('~{}/position/y'.format(waypoint_name))
+    pose.pose.position.z = rospy.get_param('~{}/position/z'.format(waypoint_name))
+
+    if(rospy.has_param('~{}/orientation/r'.format(waypoint_name))):
+        r = rospy.get_param('~{}/orientation/r'.format(waypoint_name))
+        p = rospy.get_param('~{}/orientation/p'.format(waypoint_name))
+        y = rospy.get_param('~{}/orientation/y'.format(waypoint_name))
+
+        quaternion = tf.transformations.quaternion_from_euler(r, p, y)
+
+        pose.pose.orientation.w = quaternion[0]
+        pose.pose.orientation.x = quaternion[1]
+        pose.pose.orientation.y = quaternion[2]
+        pose.pose.orientation.z = quaternion[3]
+    else:
+        pose.pose.orientation.x = rospy.get_param('~{}/orientation/x'.format(waypoint_name))
+        pose.pose.orientation.y = rospy.get_param('~{}/orientation/y'.format(waypoint_name))
+        pose.pose.orientation.z = rospy.get_param('~{}/orientation/z'.format(waypoint_name))
+        pose.pose.orientation.w = rospy.get_param('~{}/orientation/w'.format(waypoint_name))
+    return pose
+
+def get_pose(pose_name):
+    pose_covarience = PoseWithCovarianceStamped()
+    pose_stamped = get_waypoint(pose_name)
+
+    pose_covarience.header = pose_stamped.header
+    pose_covarience.pose.pose = pose_stamped.pose
+    return pose_covarience
 
 def drive(drive_speed, turn_speed):
     cmd_msg = Twist()
@@ -41,59 +79,53 @@ def drive(drive_speed, turn_speed):
     cmd_msg.angular.z = turn_speed
     vel_pub.publish(cmd_msg)
 
-def get_ramp_pose():
-    pose = PoseWithCovarianceStamped()
-    pose.header.frame_id = "map"
-    pose.pose.pose.position.x = -4.791
-    pose.pose.pose.position.y =  4.840
-    pose.pose.pose.position.z =  0.000
-    pose.pose.pose.orientation.x =  0.000
-    pose.pose.pose.orientation.y =  0.000
-    pose.pose.pose.orientation.z =  0.928
-    pose.pose.pose.orientation.w =  0.372
-    return pose
-
-def get_off_pose():
-    pose = PoseStamped()
-    pose.header.frame_id = "map"
-    pose.pose.position.x = -5.5
-    pose.pose.position.y =  5.5
-    pose.pose.position.z =  0.000
-    pose.pose.orientation.x =  0.000
-    pose.pose.orientation.y =  0.000
-    pose.pose.orientation.z =  0.928
-    pose.pose.orientation.w =  0.372
-    return pose
-
-def drive_down_ramp():
-    print("Driving to ramp...");
-    while not ramp:
-        drive(0.5, 0.0)
-    print("Driving down ramp...");
-    while ramp:
-        drive(0.5, 0.0)
-    print("Reached bottom of ramp");
-    drive(0.0, 0.0)
-
+def drive_to(waypoint):
+    nav_target.publish(waypoint)
+    while active_goal_status != 2:
+        pass
+    
+        
 # Initialization
-pose_pub = rospy.Publisher('initialpose', PoseWithCovarianceStamped, queue_size=3)
-vel_pub = rospy.Publisher('move_base_controller/cmd_vel', Twist, queue_size=3)
-imu_sub = rospy.Subscriber("imu_data", Imu, imu_callback)
-nav_target = rospy.Publisher('/move_base/current_goal', PoseStamped, queue_size=3)
-
 rospy.init_node('phoenixbot_behavior_coordinator')
 
+imu_sub = rospy.Subscriber("imu_data", Imu, imu_callback)
+nav_result = rospy.Subscriber("/move_base/result", MoveBaseActionResult, nav_callback)
+
+pose_pub = rospy.Publisher('initialpose', PoseWithCovarianceStamped, queue_size=3)
+vel_pub = rospy.Publisher('move_base_controller/cmd_vel', Twist, queue_size=3)
+nav_target = rospy.Publisher('/move_base/current_goal', PoseStamped, queue_size=3)
+
+rospy.wait_for_service('/move_base/clear_costmaps')
+clear_costmap = rospy.ServiceProxy('/move_base/clear_costmaps', Empty)
+
 # Start
+pose_pub.publish(get_pose("initial_pose"))
 wait_for_start()
 
-drive_down_ramp()
-pose_pub.publish(get_ramp_pose())
-
-drive(0.5, 0.0)
-rospy.sleep(3)
+# Drive down the ramp
+print("Driving to ramp...");
+while not ramp:
+    drive(0.5, 0.0)
+print("Driving down ramp...");
+while ramp:
+    drive(0.5, 0.0)
+print("Reached bottom of ramp");
 drive(0.0, 0.0)
 
-nav_target.publish(get_off_pose())
+# Update the pose
+pose_pub.publish(get_pose("bottom_of_ramp"))
+clear_costmap()
+rospy.sleep(3)
+
+# Drive clear of the ramp
+drive(0.5, 0.0)
+rospy.sleep(5)
+drive(0.0, 0.0)
+
+# Drive to first waypoint
+drive_to(get_waypoint("disembark"))
+
+print("FINISHED")
 
 rospy.spin()
 
