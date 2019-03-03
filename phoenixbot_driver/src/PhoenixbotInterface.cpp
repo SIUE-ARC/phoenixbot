548850b3f283622ac239a65a09cb5e629088fb71
Zach Anderson <zach.inbox@gmail.com>
2019-03-02 20:01:47 -0600

    Fixed encoder conversion logic

--- phoenixbot_driver/src/PhoenixbotInterface.cpp
+++ phoenixbot_driver/src/PhoenixbotInterface.cpp
@@ -5,13 +5,14 @@
 #define PI 3.14159
 #define DEG (PI / 180.0)
 
-#define DRIVE_PPR 1440
-#define DRIVE_REDUCTION 8.45
+#define DRIVE_PPR 360
+#define DRIVE_REDUCTION 1
 
 #define SIMON_PPR 7
-#define SIMON_REDUCTION 71.0
+#define SIMON_REDUCTION 71.0 * (22.0/16.0)
 #define SIMON_OFFSET (33 * DEG)
 
+// counts * TICKS_TO_RAD = radians
 #define TICKS_TO_RAD       ((PI * 2.0) / (DRIVE_PPR * DRIVE_REDUCTION * 4.0))
 #define TICKS_TO_RAD_SIMON ((PI * 2.0) / (SIMON_PPR * SIMON_REDUCTION * 4.0))
 
